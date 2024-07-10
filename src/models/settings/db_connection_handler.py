49d45bcfb25c12ffa81f45ee50929f47b230a013
Lucas C. Noman <lucas.noman@outlook.com>
2024-07-09 22:14:17 -0300

    feat:

---
+++ src/models/settings/db_connection_handler.py
@@ -0,0 +1,19 @@
+import sqlite3
+from sqlite3 import Connection
+
+
+class DbConnectionHandler:
+  def __init__(self) -> None:
+    self.__connection_string = 'storage.db'
+    self.__conn = None
+
+  def connect(self) -> None:
+    conn = sqlite3.connect(self.__connection_string, check_same_thread=False)
+    self.__conn = conn
+
+  def get_connection(self) -> Connection:
+    return self.__conn
+
+
+# Usarei apenas essa variável para que em todo o projeto eu tenha apenas uma conexão
+db_connection_handler = DbConnectionHandler()
