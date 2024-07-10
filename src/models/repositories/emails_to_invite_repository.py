ba40f1cc6de1ae03705fdb312e54c04fbe12ba2a
Lucas C. Noman <lucas.noman@outlook.com>
2024-07-10 06:51:31 -0300

    feat: implementing links repository with test

--- src/models/repositories/emails_to_invite_repository.py
+++ src/models/repositories/emails_to_invite_repository.py
@@ -26,5 +26,5 @@ class EmailsToInviteRepository:
   def find_emails_from_trip(self, trip_id: str) -> List[Tuple]:
     cursor = self.__conn.cursor()
     cursor.execute("""SELECT * FROM emails_to_invite WHERE trip_id = ?""", (trip_id,))
-    trip = cursor.fetchall()
-    return trip
+    email = cursor.fetchall()
+    return email
---
+++ src/models/repositories/link_repository_test.py
@@ -0,0 +1,37 @@
+import uuid
+
+import pytest  # type: ignore
+
+from src.models.repositories.link_repository import LinksRepository
+from src.models.settings.db_connection_handler import db_connection_handler
+
+db_connection_handler.connect()
+
+trip_id = str(uuid.uuid4())
+link_id = str(uuid.uuid4())
+
+
+@pytest.mark.skip(reason='Interação com o banco')
+def test_registry_link():
+  conn = db_connection_handler.get_connection()
+  links_repository = LinksRepository(conn)
+
+  link_infos = {
+    'id': link_id,
+    'trip_id': trip_id,
+    'link': 'https://www.google.com',
+    'title': 'Google',
+  }
+
+  links_repository.registry_link(link_infos)
+
+
+@pytest.mark.skip(reason='Interação com o banco')
+def test_find_links_from_trip():
+  conn = db_connection_handler.get_connection()
+  links_repository = LinksRepository(conn)
+
+  links = links_repository.find_links_from_trip(trip_id)
+
+  assert isinstance(links, list)
+  assert isinstance(links[0], tuple)
