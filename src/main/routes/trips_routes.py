4de0206bde8e9d1b7fbcfd23cdd6ac5f8cb3a984
Lucas C. Noman <lucas.noman@outlook.com>
2024-07-09 21:40:27 -0300

    refactor: adding first test route to project

---
+++ src/main/routes/trips_routes.py
@@ -0,0 +1,8 @@
+from flask import jsonify, Blueprint
+
+trips_routes_bp = Blueprint('trip_routes', __name__)
+
+
+@trips_routes_bp.route('/trips', methods=['GET'])
+def create_trip():
+  return jsonify({'Hello': 'there!'}), 200
